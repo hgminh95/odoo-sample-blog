d03b20d95a5ff1a0bb7ee9d6b59b396b1f668948
Minh Hoang Gia <hgminh95@gmail.com>
2016-07-22 16:03:56 +0700

    Add workflow for article

--- __openerp__.py
+++ __openerp__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/article_views.xml',
         'views/templates.xml',
+        'data/article_workflow.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/article.py
+++ models/article.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class article(models.Model):
@@ -8,3 +8,20 @@ class article(models.Model):
 
     title = fields.Char(string="Title", required=True)
     content = fields.Text(string="Content")
+    state = fields.Selection(string="State", selection=[
+        ("draft", "Draft"),
+        ("review", "In Review"),
+        ("done", "Done")
+    ])
+
+    @api.multi
+    def action_draft(self):
+        self.state = 'draft'
+
+    @api.multi
+    def action_review(self):
+        self.state = 'review'
+
+    @api.multi
+    def action_done(self):
+        self.state = 'done'
